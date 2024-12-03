907cd86f18231a1367ab66c01844305ec86c6bce
Konstantinos Lyroudis <s232994@dtu.dk>
2024-12-03 15:20:51 +0100

    Added Computerome MapReduce file

---
+++ Computerome MapReduce/collector.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import os
+import pandas as pd
+
+# This is the collector for MapReduce. Collects the results from every worker and creates the final merged output file.
+
+# Collector function to merge processed chunks
+def collector(output_dir, final_output_file):
+    # List all output files that match the new pattern
+    output_files = sorted(
+        [f for f in os.listdir(output_dir) if f.endswith('.tsv_out.tsv')],
+        key=lambda x: int(x.split('_')[1].split('.')[0])  # Sort by sequence number
+    )
+
+    # Read all files and add them to a list of DataFrames
+    df_list = []
+    for filename in output_files:
+        file_path = os.path.join(output_dir, filename)
+        df = pd.read_csv(file_path, sep='\t', header=None)
+        df_list.append(df)
+
+    # Concatenate all DataFrames
+    if df_list:
+        final_df = pd.concat(df_list, ignore_index=True)
+        final_df.to_csv(final_output_file, sep='\t', index=False, header=False)
+
+if __name__ == "__main__":
+    collector("/home/projects/pr_course/people/konlyr/tools/split_chunks",
+              "/home/projects/pr_course/people/konlyr/tools/final_cleaned_dataset.tsv")
---
+++ Computerome MapReduce/jobscript_adm.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# This is the jobscript file to submit the administrator.py to the queueing system of Computerome
+
+#PBS -W group_list=pr_course -A pr_course
+#PBS -l nodes=1:ppn=1:thinnode
+#PBS -l mem=10GB
+#PBS -l walltime=3:00:00
+#PBS -e /home/projects/pr_course/people/konlyr/tools/tools.err
+#PBS -o /home/projects/pr_course/people/konlyr/tools/tools.out
+#PBS -N tools
+#PBS -M s232994@dtu.dk
+#PBS -m abe
+
+# Load required modules
+module load python36
+
+# Set up and activate virtual environment
+VENV_DIR="$HOME/myenv"
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv $VENV_DIR
+fi
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate
+
+# Upgrade pip and install necessary packages
+pip install --upgrade pip
+pip install joblib pandas nltk
+
+# Download necessary NLTK data
+python -c "
+import nltk
+nltk.download('stopwords', download_dir='$VENV_DIR/nltk_data')
+nltk.download('wordnet', download_dir='$VENV_DIR/nltk_data')
+nltk.download('averaged_perceptron_tagger', download_dir='$VENV_DIR/nltk_data')
+nltk.download('punkt', download_dir='$VENV_DIR/nltk_data')
+nltk.download('omw-1.4', download_dir='$VENV_DIR/nltk_data')
+"
+
+# Set the NLTK_DATA environment variable to use the downloaded data
+export NLTK_DATA="$VENV_DIR/nltk_data"
+
+# Run the administrator script using the Python from the virtual environment
+python /home/projects/pr_course/people/konlyr/tools/administrator.py
